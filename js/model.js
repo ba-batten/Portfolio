e720a64d196305b2cdd5357e6ee9d4638a98a76f
Brandon Batten <ba_batten@live.com>
2017-10-25 22:07:02 -0400

    Run script after document loads

--- js/model.js
+++ js/model.js
@@ -1,20 +1,28 @@
-var data = {
-  aboutMe: [
-    {
-      heading: 'What I Can Do',
-      content: ''
-    },
-    {
-      heading: 'Who I Am',
-      content: ''
-    },
-    {
-      heading: 'Check Out My Blog',
-      content: ''
-    },
-    {
-      heading: 'How To Get Up With Me',
-      content: ''
-    },
-  ]
-}
+$(document).ready(
+  function AppViewModel() {
+    var self = this;
+
+    self.aboutMe = ko.observableArray([
+      {
+        heading: 'What I Can Do',
+        content: ''
+      },
+      {
+        heading: 'Who I Am',
+        content: ''
+      },
+      {
+        heading: 'Check Out My Blog',
+        content: ''
+      },
+      {
+        heading: 'How To Get Up With Me',
+        content: ''
+      },
+    ]);
+  };
+);
+
+var model = new AppViewModel();
+
+ko.applyBindings(model);
