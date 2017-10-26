ee89c7c2b62482791a37fb69f9c141f77c31a954
Brandon Batten <ba_batten@live.com>
2017-10-25 22:50:48 -0400

    Move aboutMe into an observableArray

--- js/model.js
+++ js/model.js
@@ -1,27 +1,25 @@
-$(document).ready(
-  function AppViewModel() {
-    var self = this;
+function AppViewModel() {
+  var self = this;
 
-    self.aboutMe = ko.observableArray([
-      {
-        heading: 'What I Can Do',
-        content: ''
-      },
-      {
-        heading: 'Who I Am',
-        content: ''
-      },
-      {
-        heading: 'Check Out My Blog',
-        content: ''
-      },
-      {
-        heading: 'How To Get Up With Me',
-        content: ''
-      },
-    ]);
-  };
-);
+  self.aboutMe = ko.observableArray([
+    {
+      heading: 'What I Can Do',
+      content: ''
+    },
+    {
+      heading: 'Who I Am',
+      content: ''
+    },
+    {
+      heading: 'Check Out My Blog',
+      content: ''
+    },
+    {
+      heading: 'How To Get Up With Me',
+      content: ''
+    },
+  ]);
+};
 
 var model = new AppViewModel();
 
