4524fef84dfe84241e2d2987d3c5408ae35b27f3
Brandon Batten <ba_batten@live.com>
2017-10-25 22:02:37 -0400

    Add an array with information to fill About Me section

---
+++ js/model.js
@@ -0,0 +1,20 @@
+var data = {
+  aboutMe: [
+    {
+      heading: 'What I Can Do',
+      content: ''
+    },
+    {
+      heading: 'Who I Am',
+      content: ''
+    },
+    {
+      heading: 'Check Out My Blog',
+      content: ''
+    },
+    {
+      heading: 'How To Get Up With Me',
+      content: ''
+    },
+  ]
+}
