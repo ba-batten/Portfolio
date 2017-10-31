d021a47f6546064d09b1b6a758dbbe70d2851c00
Brandon Batten <ba_batten@live.com>
2017-10-30 22:02:28 -0400

    Replace all images in prevWork with square image

--- js/model.js
+++ js/model.js
@@ -38,9 +38,7 @@ function AppViewModel() {
     {
       name: "Neighborhood Map",
       url: "https://github.com/ba-batten/NeighborhoodMap",
-      smallPic: "images/neighborhood-map-370.jpeg",
-      medPic: "images/neighborhood-map-640.jpeg",
-      largePic: "images/neighborhood-map.jpeg",
+      pic: "images/neighborhood-map-square.jpeg",
       description: "A single-page web application, built using the Knockout framework," +
         " that displays a Google Map of an area and various points of interest. Users can" +
         " search all included landmarks and, when selected, additional information about" +
@@ -52,9 +50,7 @@ function AppViewModel() {
     {
       name: "Arcade Clone",
       url: "https://github.com/ba-batten/ClassicArcadeGameClone",
-      smallPic: "images/frogger-clone_370.jpeg",
-      medPic: "images/frogger-clone_640.jpeg",
-      largePic: "images/frogger-clone.jpeg",
+      pic: "images/frogger-clone-square.jpeg",
       description: "An HTML5 Canvas powered video game, developed using the best practices" +
         " in Object Oriented JavaScript.",
       id: "#arcadeCloneModal",
@@ -64,9 +60,7 @@ function AppViewModel() {
     {
       name: "Weather Widget",
       url: "https://codepen.io/brandonbatten/pen/eGXEEB",
-      smallPic: "images/frogger-clone_370.jpeg",
-      medPic: "images/frogger-clone_640.jpeg",
-      largePic: "images/frogger-clone.jpeg",
+      pic: "images/weather-square.jpeg",
       description: "Weather widget that makes AJAX calls to get the current location," +
         " and weather conditions to display along with the date and time.",
       id: "#weatherWidgetModal",
@@ -76,9 +70,7 @@ function AppViewModel() {
     {
       name: "Quote Machine",
       url: "https://codepen.io/brandonbatten/pen/oGqJGE",
-      smallPic: "images/frogger-clone_370.jpeg",
-      medPic: "images/frogger-clone_640.jpeg",
-      largePic: "images/frogger-clone.jpeg",
+      pic: "images/quote-machine-square.jpeg",
       description: "Randomly displays quotes from the popular TV show \"The Office\"." +
         " User is able to tweet quotes.",
       id: "#quoteMachineModal",
@@ -88,9 +80,7 @@ function AppViewModel() {
     {
       name: "Wikipedia Viewer",
       url: "https://codepen.io/brandonbatten/pen/jGegZY",
-      smallPic: "images/frogger-clone_370.jpeg",
-      medPic: "images/frogger-clone_640.jpeg",
-      largePic: "images/frogger-clone.jpeg",
+      pic: "images/wiki-viewer-square.jpeg",
       description: "Takes search term to make AJAX call to Wikipedia API to search for" +
         " Wikipedia articles",
       id: "#wikipediaViewerModal",
@@ -100,9 +90,7 @@ function AppViewModel() {
     {
       name: "Website Optimization",
       url: "https://github.com/ba-batten/WebsiteOptimization",
-      smallPic: "images/pizzeria-370.jpeg",
-      medPic: "images/pizzeria-640.jpeg",
-      largePic: "images/pizzeria.jpeg",
+      pic: "images/pizzeria-square.jpeg",
       description: "Optimized an inefficient web application's JavaScript, CSS and" +
         " assets delivery, ensuring it runs at 60fps and achieves a PageSpeed score" +
         " of at least 90.",
@@ -113,9 +101,7 @@ function AppViewModel() {
     {
       name: "Feed Reader Testing",
       url: "https://github.com/ba-batten/FeedReaderTesting",
-      smallPic: "images/feed-reader-370.jpeg",
-      medPic: "images/feed-reader-640.jpeg",
-      largePic: "images/feed-reader.jpeg",
+      pic: "images/feed-reader-square.jpeg",
       description: "Wrote comprehensive unit tests, using the Jasmine testing" +
         " framework, for an RSS Feed Reader application that uses Google's RSS API.",
       id: "#feedReaderModal",
