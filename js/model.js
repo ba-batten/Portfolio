bf7e802aadbc91140dbf06323e20b25a46f93968
Brandon Batten <ba_batten@live.com>
2017-11-02 22:40:18 -0400

    Edit about me information to be shorter

--- js/model.js
+++ js/model.js
@@ -15,15 +15,14 @@ function AppViewModel() {
       heading: 'Who I Am',
       content: 'I built my first web site in high school and have carried a passion for web development ' +
         'ever since.  There is not a day that goes by that I am not writing code or planning out a new project.' +
-        '  You can find me hanging out with my family, taking in a nice day watching football, or knocking ' +
-        ' around golf balls when I\'m not spending quality time with my laptop and favorite text-editor .'
+        '  You can find me hanging out with my family, taking in a football game, or knocking ' +
+        ' around some golf balls when I\'m not spending quality time with my laptop and favorite text-editor .'
     },
     {
       heading: 'What I Can Do',
-      content: 'My tech tool bag consists of HTML, CSS, and JavaScrtip.  I use several frameworks and libraries ' +
+      content: 'My tech tool bag consists of HTML, CSS, and JavaScript.  I use several frameworks and libraries ' +
         ' such as Bootstrap, jQuery, and Knockout JS.  I design test suites with Jasmine.  Some other ' +
-        'technologies I take advantage of are Git for version control and Adobe products like' +
-        'Photoshop and Illustrator.  As I grow as a developer, I will be adding more tools to my tool bag.'
+        'technologies I take advantage of are Git for version control and Adobe products like Photoshop and Illustrator.'
     },
     {
       heading: 'Check Out My Blog',
